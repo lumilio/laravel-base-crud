7527e1ef660e31157ee7d07bc48abbae60ffee9b
lumilio <logistic-division@lumilium.com>
2022-01-21 05:11:58 +0100

    Update ComicSeeder

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $comics_arrey = config('(2)comics');
+        foreach ($comics_arrey as $item){
+            $_item = new Comic();
+            $_item -> title = $item['title'];
+            $_item -> description = $item['description'];
+            $_item -> thumb = $item['thumb'];
+            $_item -> price = $item['price'];
+            $_item -> series = $item['series'];
+            $_item -> sale_date = $item['sale_date'];
+            $_item -> type = $item['type'];
+            $_item -> save();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('posts', 'Admin\PostController@index');
-Route::get('posts/create', 'Admin\PostController@create');
-Route::post('posts', 'Admin\PostController@store');
-Route::get('posts/{post}', 'Admin\PostController@show');
-Route::get('posts/{post}/edit', 'AdminnPostController@edit');
-Route::put('posts/{post}', 'Admin\PostController@update');
-Route::delete('posts/{post}', 'Admin\PostController@destroy');
+
+Route::get('posts', 'Admin\ComicController@index');
+Route::get('posts/create', 'Admin\ComicController@create');
+Route::post('posts', 'Admin\ComicController@store');
+Route::get('posts/{post}', 'Admin\ComicController@show');
+Route::get('posts/{post}/edit', 'Admin\ComicController@edit');
+Route::put('posts/{post}', 'Admin\ComicController@update');
+Route::delete('posts/{post}', 'Admin\ComicController@destroy');
 
 
 
